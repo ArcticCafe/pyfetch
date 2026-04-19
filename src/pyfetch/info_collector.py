3af4ee7a2075949928fa94a4ed522c6fda4b679d
Shenyi Huang <3180102081@zju.edu.cn>
2026-04-19 21:05:47 +0000

    backup before the server dies

--- src/pyfetch/info_collector.py
+++ src/pyfetch/info_collector.py
@@ -1,13 +1,53 @@
-import os
 import platform
+
+from pydantic import BaseModel
+
+
+class CPUInfo(BaseModel):
+    model_name: str
+    cores: int
+    cpu_freq: float
+    raw_data: dict[str, str]
+
+
+class OSInfo(BaseModel):
+    distro_name: str
+    kernel_version: str
+    architecture: str
+    system: str
+    server_name: str
+
+
+def _get_os_info_object() -> OSInfo:
+    return OSInfo(
+        distro_name=platform.freedesktop_os_release().get("PRETTY_NAME", "Unknown OS"),
+        kernel_version=platform.release(),
+        architecture=platform.machine(),
+        system=platform.system(),
+        server_name=platform.node(),
+    )
+
+
+def _get_first_core_raw_data() -> dict:
+    """Parses /proc/cpuinfo and return a dictionary of the first core's raw data."""
+    current_core = {}
+    with open("/proc/cpuinfo", "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                if current_core:
+                    return current_core
+            key, value = line.split(":", 1)
+            current_core[key.strip()] = value.strip()
+    return current_core if current_core else {}
 
 
 class InfoCollector:
-    def __init__(self): ...
+    def __init__(self):
+        self.os_info = _get_os_info_object()
 
     def _get_os_release(self):
-        os_info = platform.freedesktop_os_release()
-        return os_info.get("PRETTY_NAME", "Unknown OS")
+        return self.os_info.distro_name
 
     def _get_cpu_info(self):
         cpu_info = platform.machine()
@@ -20,3 +60,9 @@ class InfoCollector:
     @property
     def cpu_info(self):
         return self._get_cpu_info()
+
+
+if __name__ == "__main__":
+    collector = InfoCollector()
+    print(collector.os_release)
+    print(collector.cpu_info)
