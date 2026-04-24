e5ef14e6f8c5774d32e2c1151ecd15cfedcfd23e
Shenyi Huang <3180102081@zju.edu.cn>
2026-04-23 23:33:31 -0500

    use rich for the printer

--- src/pyfetch/printer.py
+++ src/pyfetch/printer.py
@@ -1,15 +1,22 @@
+from dis import show_code
+
 import rich
+from rich.align import Align
 
 from pyfetch.info_collector import InfoCollector
 
 
 class Printer:
-    def __init__(self): ...
+    def __init__(self):
+        self.console = rich.get_console()
 
     def print(self, info_dict):
-        for key, value in info_dict.items():
-            print(key)
-            print(value)
+        for key, dicts in info_dict.items():
+            self.console.rule(
+                key,
+            )
+            for sub_key, sub_value in dicts.items():
+                self.console.print(Align.right(f"{sub_key}: {sub_value}"))
 
 
 if __name__ == "__main__":
